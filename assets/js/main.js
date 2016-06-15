1c14ef77a02a700360527cb2fe099fd5691eceaa
NicholasPiano <nicholas.d.piano@gmail.com>
2016-06-16 00:19:16 +0100

    load with jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+$(document).ready(function () {
 	// initialise
 	if ($(window).width() < 800) {
 		$('.half').addClass('m');
@@ -66,12 +66,12 @@
 		// these HTTP methods do not require CSRF protection
 		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 	}
-	$.ajaxSetup({
-			beforeSend: function(xhr, settings) {
-					if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-							xhr.setRequestHeader("X-CSRFToken", csrftoken);
-					}
-			}
-	});
 
-})();
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		}
+	});
+});
