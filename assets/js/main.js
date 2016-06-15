5e08b1188115c39c8c187968e6f4443dfc0b3286
NicholasPiano <nicholas.d.piano@gmail.com>
2016-06-16 00:46:11 +0100

    try using cookies.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
-// using jQuery
-function getCookie(name) {
-	console.log('cookie');
-	var cookieValue = null;
-	if (document.cookie && document.cookie != '') {
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = jQuery.trim(cookies[i]);
-			// Does this cookie string begin with the name we want?
-			if (cookie.substring(0, name.length + 1) == (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	return cookieValue;
-}
-
 $(document).ready(function () {
 	// initialise
 	if ($(window).width() < 800) {
@@ -63,6 +45,6 @@ $(document).ready(function () {
 	});
 
 	$.ajaxSetup({
-		headers: { "X-CSRFToken": getCookie("csrftoken") }
+		headers: { "X-CSRFToken": Cookies.get("csrftoken") }
 	});
 });
