f60db5a0534bc8403b8009f7cd8539090901f62c
NicholasPiano <nicholas.d.piano@gmail.com>
2016-06-16 00:36:24 +0100

    try function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+// using jQuery
+function getCookie(name) {
+	console.log('cookie');
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+
 $(document).ready(function () {
 	// initialise
 	if ($(window).width() < 800) {
@@ -43,23 +61,6 @@ $(document).ready(function () {
 		$('input#email').val('');
 		$('textarea#message').val('Thanks for getting in touch. Someone will respond to your enquiry soon!');
 	});
-
-	// using jQuery
-	function getCookie(name) {
-		var cookieValue = null;
-		if (document.cookie && document.cookie != '') {
-			var cookies = document.cookie.split(';');
-			for (var i = 0; i < cookies.length; i++) {
-				var cookie = jQuery.trim(cookies[i]);
-				// Does this cookie string begin with the name we want?
-				if (cookie.substring(0, name.length + 1) == (name + '=')) {
-					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-					break;
-				}
-			}
-		}
-		return cookieValue;
-	}
 
 	$.ajaxSetup({
 		headers: { "X-CSRFToken": getCookie("csrftoken") }
